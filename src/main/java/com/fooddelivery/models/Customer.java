dc5251898bc01602fb3b3ccc031e1f3fe2b6cab4
Liam Baker <liam.baker2310@gmail.com>
2024-04-07 19:21:47 +0900

    refactored and made spacing consistent

--- src/main/java/com/fooddelivery/models/Customer.java
+++ src/main/java/com/fooddelivery/models/Customer.java
@@ -3,15 +3,6 @@ package com.fooddelivery.models;
 import com.fooddelivery.utils.Column;
 
 public class Customer {
-	@Column(name = "id", nullable = false)
+	@Column(name = "id", nullable = false, autoincrement = true, primaryKey = true)
 	private int id;
-
-	/**
-	 * Constructs a new Customer instance.
-	 *
-	 * @param id The unique identifier of the meal.
-	 */
-	public Customer(int id) {
-		this.id = id;
-	}
 }
--- src/main/java/com/fooddelivery/models/Employee.java
+++ src/main/java/com/fooddelivery/models/Employee.java
@@ -3,6 +3,6 @@ package com.fooddelivery.models;
 import com.fooddelivery.utils.Column;
 
 public class Employee {
-    @Column(name = "id", nullable = false)
-    private int id;
+	@Column(name = "id", nullable = false, autoincrement = true, primaryKey = true)
+	private int id;
 }
